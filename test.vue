e0254d25582b3e7187af99e67f310a656047a575
KhadijaDas13079 <137698191+KhadijaDas13079@users.noreply.github.com>
2023-06-25 20:26:04 +0800

    Create test

---
+++ test.vue
@@ -0,0 +1,12 @@
+interface ComponentOptions {
+  inject?: ArrayInjectOptions | ObjectInjectOptions
+}
+
+type ArrayInjectOptions = string[]
+
+type ObjectInjectOptions = {
+  [key: string | symbol]:
+    | string
+    | symbol
+    | { from?: string | symbol; default?: any }
+}
